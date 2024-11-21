16e371a12e3dda8363b438efebaa3e351fbef145
vlad99 <@VlAdOsAn>
2024-11-22 02:31:22 +0300

    Делаю по чуть-чуть домашку №13(вроде), не доделал контекстный менеджер

--- tests/test.py
+++ tests/test.py
@@ -1,27 +1,34 @@
 import pytest
+import allure
+
+from PetProject.Pet.main import pet_id, status_code
 from PetProject.Pet.pet_method import PetMethod
 
-class TestSubPet:
-    pet_test = PetMethod()
-    def setup_method(self): #Создаем питомца, дергаем его id для проверок
-        pet, status_code = self.pet_test.add_pet(
-            name="Boby",
-            status="available",
-            category_name="cat",
-            tag="little"
-        )
-        self.pet_id = pet['id']
-        print("ID питомца:", self.pet_id)
-        self.status_code = status_code
-    def test_add_pet(self):
-        pet = self.pet_test.get_pet(self.pet_id)
-        assert pet is not None
-        assert self.status_code == 200
+@allure.epic('Тестирвоание API')
+@allure.feature('Проверка метода TestSubPet')
 
-    def test_del_pet(self):
-        del_status_code = self.pet_test.delete_pet(self.pet_id)
-        self.status_code = del_status_code
-        assert self.status_code == 200
+# Это нам не нужно
+# class TestSubPet:
+#     pet_test = PetMethod()
+#     def setup_method(self): #Создаем питомца, дергаем его id для проверок
+#         pet, status_code = self.pet_test.add_pet(
+#             name="Boby",
+#             status="available",
+#             category_name="cat",
+#             tag="little"
+#         )
+#         self.pet_id = pet['id']
+#         print("ID питомца:", self.pet_id)
+#         self.status_code = status_code
+#     def test_add_pet(self):
+#         pet = self.pet_test.get_pet(self.pet_id)
+#         assert pet is not None
+#         assert self.status_code == 200
+#
+#     def test_del_pet(self):
+#         del_status_code = self.pet_test.delete_pet(self.pet_id)
+#         self.status_code = del_status_code
+#         assert self.status_code == 200
 
 
 # Мне не понравилось, что каждый раз добавляется новый питомец
@@ -44,18 +51,46 @@ def pet_setup():
 class TestSubPet:
     def setup_method(self):
         self.pet_test = PetMethod()
+
+    @allure.story('Проверка метода добавления')
     def test_add_pet(self, pet_setup):
-        pet_id, status_code = pet_setup
-        pet = self.pet_test.get_pet(pet_id)
-        assert pet is not None
-        assert status_code == 200
+        with allure.step('Получаем данные из фикстуры'):
+            pet_id, status_code = pet_setup
+        with allure.step('Получаем информацию о питомце по ID'):
+            pet = self.pet_test.get_pet(pet_id)
+        with allure.step('Проверяем, что питомец успешно создан'):
+            assert pet is not None
+        with allure.step('Проверяем, что статус ответа равен 200'):
+            assert status_code == 200
+        with allure.step('Добавляем аттач'):
+            with open("my_pet.json", "r", encoding="utf8") as file:
+                json_content = file.read()
+                allure.attach(json_content, name="JSON Data", attachment_type=allure.attachment_type.JSON)
+
+    @allure.story('Проверка метода получения и записи в JSON')
+    def test_get_pet(self, pet_setup):
+        with allure.step('Получаем данные из фикстуры'):
+            pet_id, status_code = pet_setup
+        with allure.step('Получаем информацию о питомце по ID'):
+            pet = self.pet_test.get_pet(pet_id)
+        with allure.step('Проверяем, что питомец успешно найден'):
+            assert pet is not None
+        with allure.step('Проверяем, что статус ответа равен 200'):
+            assert status_code == 200
+
+    @allure.story('Проверка метода удаления')
     def test_del_pet(self, pet_setup):
-        pet_id, _ = pet_setup
-        del_status_code = self.pet_test.delete_pet(pet_id)
-        assert del_status_code == 200
-    def test_get_del_pet(self, pet_setup):
-        pet_id, status_code = pet_setup
-        pet = self.pet_test.get_pet(pet_id)
-        assert pet == 'Eror'
-        assert status_code == 200 #Должна быть 404 ошибка,
+        with allure.step('Получаем ID питомца из фикстуры'):
+            pet_id, _ = pet_setup
+        with allure.step('Усыпляем питомца'):
+            del_status_code = self.pet_test.delete_pet(pet_id)
+        with allure.step('Проверяем, что статус ответа равен 200'):
+            assert del_status_code == 200
+
+    # Это нам не нужно
+    # def test_get_del_pet(self, pet_setup):
+    #     pet_id, status_code = pet_setup
+    #     pet = self.pet_test.get_pet(pet_id)
+    #     assert pet == 'Eror'
+    #     assert status_code == 200 #Должна быть 404 ошибка,
         # но постоянно выкидывает ошибку, что фактический статус код == 200
